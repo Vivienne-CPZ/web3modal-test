459fdc446b654b7028370b15ceb447cecf5e5c24
VivienneChen <viviennechen@cherrypicks.com>
2022-12-14 17:41:52 +0800

    add console.log message

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './App.css';
 
 // Web3Modal
 import { useWeb3Modal } from "@web3modal/react";
+import { useSignMessage, useAccount, useDisconnect } from "wagmi";
 
 function App() {
   const { open: web3ModalOpen } = useWeb3Modal();
+  const { isConnected, address } = useAccount();
+  console.log(isConnected, address)
   return (
     <div className="App">
      <IonButton  onClick={() => web3ModalOpen()}>
